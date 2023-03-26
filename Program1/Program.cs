31068fca4030d37bec99b1fc99018e9e8b2df54e
One Outofthethree <hku1up@outlook.com>
2023-03-27 01:06:32 +0300

    added 1st commit

---
+++ Program1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int a= int.Parse(Console.ReadLine());
+if (a>99 & a<1000)
+{
+    int result = a/10%10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число.");
+}
