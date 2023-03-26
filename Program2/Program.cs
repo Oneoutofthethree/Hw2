a893b9b95e60d44ec7db11052dd0a14646d178ee
One Outofthethree <hku1up@outlook.com>
2023-03-27 01:08:18 +0300

    added 2nd program

---
+++ Program2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число:");
+
+int a = int.Parse(Console.ReadLine());
+if(a<100)
+{
+    Console.WriteLine("Введите число ,более чем двухзначное.");
+}
+if(a<1000 & a>99)
+{
+    int result = a%10;
+    Console.WriteLine(result);
+}
+while (a>999)
+{
+    a = a/10;
+        if (a<1000)
+        {
+            int result = a%10;
+            Console.WriteLine(result);
+        }
+}
