c944c484adbbf473cccfc05eb78f63117b4ac69e
One Outofthethree <hku1up@outlook.com>
2023-03-27 01:10:06 +0300

    added 3rd program

---
+++ Program3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+if (a>7|a<1)
+{
+    Console.WriteLine("Введенное не является номером дня недели.");
+}
+if(a==7|a==6)
+    {
+        Console.WriteLine("Выходной");
+    }
